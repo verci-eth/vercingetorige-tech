6e3fe4fdfe1b5635682c2bed4a8a9b46b9ec33b8
verci-eth <hello@vercingetorige.tech>
2023-02-15 00:23:20 +0100

    web-46 copy logo invert color

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            algotrader building emotionless trading strategies
           </p>
           <div>
             <a
@@ -48,10 +47,10 @@ export default function Home() {
           />
           <div className={styles.thirteen}>
             <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
+              src="/logo.png"
+              alt="logo"
+              width={75}
+              height={75}
               priority
             />
           </div>
